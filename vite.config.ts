e7047475ec0efb120609f03fd500a5fa2da3f0e5
Zoody <reopard506@gmail.com>
2024-11-15 15:35:22 +0900

    [🔨CHORE]     : 개발환경 수정    - path/alias 를 위해 @types/node 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
+import path from "path";
 import babel from "vite-plugin-babel";
 
 export default defineConfig({
+  esbuild: {
+    jsxFactory: "Peact.createElement",
+    jsxFragment: "Peact.Fragment",
+    jsxInject: `import Peact from "@core/peact"`,
+  },
   plugins: [
     babel({
       babelConfig: {
@@ -13,6 +19,9 @@ export default defineConfig({
             "@babel/plugin-transform-react-jsx",
             {
               runtime: "automatic", // 자동으로 pragma를 사용하도록 설정
+              // importSource: "src/core/peact", // Peact의 경로를 지정
+              // pragma: "Peact.createElement",
+              // pragmaFrag: "Preact.Fragment",
               throwIfNamespace: false, //  JSX 스펙에서는 허용되지만 React에서는 허용되지 않는 JSX인 경우 에러를 throw할건지 말건지 결정하는 옵션
             },
           ],
@@ -25,4 +34,10 @@ export default defineConfig({
     target: "esnext",
   },
   base: "/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@core": path.resolve(__dirname, "./src/core"),
+    },
+  },
 });
