1d821220764f0fc793dca7df426961c52dd70f01
Zoody <reopard506@gmail.com>
2024-11-30 22:05:00 +0900

    [🎨STYLE]     : React 의 useEffect 의 의미에 맞게 callback 매개변수 이름 변경    - callback -> setupFn

--- src/core/useEffect.ts
+++ src/core/useEffect.ts
@@ -1,7 +1,7 @@
 import { PeactValueUI } from "./PeactValueUI";
 import { isEqual } from "./utils";
 
-export function useEeffect(callback: () => void | (() => void), deps: any[]) {
+export function useEeffect(setupFn: () => void | (() => void), deps: any[]) {
   const oldHook =
     PeactValueUI.wipFiber.alternate &&
     PeactValueUI.wipFiber.alternate.hooks &&
@@ -17,7 +17,7 @@ export function useEeffect(callback: () => void | (() => void), deps: any[]) {
   };
 
   if (!oldHook || !isEqual(oldHook.deps, deps)) {
-    hook.cleaner = callback() || undefined; // 클린업 함수 저장
+    hook.cleaner = setupFn() || undefined; // 클린업 함수 저장
   }
 
   if (PeactValueUI.wipFiber.hooks) {
